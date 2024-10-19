ff39df0805cb9de53e9d1de01e6b2ce378db09c6
MaXLin1998 <33195455+MaXLin1998@users.noreply.github.com>
2024-10-19 13:33:26 +0900

    Update schema.sql

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,11 +1,11 @@
-CREATE TABLE ORDERS (
-    order_id int NOT NULL PRIMARY KEY,
-    order_date DATE DEFAULT sysdate NOT NULL,
-    company_no INT DEFAULT 0,
-    company_name VARCHAR(255),
-    item_no INT DEFAULT 0,
+CREATE TABLE IF NOT EXISTS public.ORDERS (
+     order_id bigint NOT NULL,
+     order_date timestamp NOT NULL,
+     company_no bigint DEFAULT 0,
+     company_name VARCHAR(255),
+    item_no bigint DEFAULT 0,
     item VARCHAR(255),
-    quantity INT DEFAULT 0,
-    unit_price INT DEFAULT 0,
-    price INT DEFAULT 0
+    quantity bigint DEFAULT 0,
+    unit_price bigint DEFAULT 0,
+    price bigint DEFAULT 0
 );
